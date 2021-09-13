66c96caf51afc8188f395c35adbf17c645f8960f
Gerionil <67752928+Gerionil@users.noreply.github.com>
2021-09-13 16:02:13 +0300

    initial commit

--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes } from "../../utils/routes";
 import { Link } from "react-router-dom";
-import './Registration.scss'
+import './Registration.scss';
 
 const Registration = () => {
 	return (
